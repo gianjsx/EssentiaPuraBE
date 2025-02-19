3ef1b5f6f4f5ce7eb6bed3aeee7e611d1562a4d2
Gianjsx <giancarlodp74@gmail.com>
2025-02-18 18:06:08 -0600

    2 commit

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config();
 
 export default {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 6000,
   CLOUD_NAME: process.env.CLOUD_NAME,
   API_KEY: process.env.API_KEY,
   API_SECRET: process.env.API_SECRET,
